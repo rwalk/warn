40a5346b4406152e31d6e731ee7b719f638b3a45
Ryan Walker <ryan@ryanwalker.us>
2016-08-06 15:23:50 -0400

    loggers fix

--- logger.py
+++ logger.py
@@ -4,5 +4,5 @@ class WARNLogger:
 
     @staticmethod
     def get_logstash_handler():
-        handler = RotatingFileHandler("/var/log/warn/warn.log", maxBytes=1000)
+        handler = RotatingFileHandler("logs/warn.log", maxBytes=1000)
         return handler
