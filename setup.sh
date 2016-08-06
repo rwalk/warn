29840a8dbdf5a375c19e208b3f82b1bda9822fc2
Ryan Walker <ryan@ryanwalker.us>
2016-08-06 15:35:22 -0400

    forgot the static assets

--- setup.sh
+++ setup.sh
@@ -15,8 +15,12 @@ git clone https://github.com/rwalk/warn
 # docker compose
 sudo pip3 install docker-compose
 
+# map 80 to 8080
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 # reboot
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo reboot now
+
+
